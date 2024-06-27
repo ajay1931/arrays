3f059e451d704c7fa631561ee2342a861814298c
ajay1931 <ajayprakash032@gmail.com>
2024-06-27 09:03:30 +0530

    finding array

---
+++ finding_array.js
@@ -0,0 +1,25 @@
+let items = [
+    { id: 1, name: "Android Mobiles", cost: 9000 },
+    { id: 2, name: "Iphone Mobiles", cost: 25000 },
+    { id: 3, name: "Windows Mobiles", cost: 11000 },
+    { id: 4, name: "Ipad", cost: 20000 },
+    { id: 5, name: "Mobiles", cost: 15000 },
+];
+
+// Find method
+function findPrice() {
+    let price = items.find(value => {
+        return value.cost < 12000;
+    })
+    console.log(price);
+}
+findPrice()
+
+// FindIndex method
+function findPrice1() {
+    let price = items.findIndex(value => {
+        return value.cost < 12000;
+    })
+    console.log(price);
+}
+findPrice1()
