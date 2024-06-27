2ea90bee894ed87f6c0b24fadd4d4c811bab0216
ajay1931 <ajayprakash032@gmail.com>
2024-06-27 09:02:51 +0530

    updated

--- filtering_array.js
+++ filtering_array.js
@@ -1,17 +1,37 @@
-let age = [15,34,24,14,56,33];
+// Primitive type
 
-function isAdults(){
-    let adult = age.filter((value)=>{
-        return value >18;
-    });
-    console.log("Adults:",adult);
-}
-isAdults() 
+let age = [15, 34, 24, 14, 56, 33];
 
-function iseven(){
-    let even= age.filter(value=>{
-        return value %2==0;
+function isAdults() {
+    let adult = age.filter((value) => {
+        return value > 18;
     });
-    console.log("Even numbers:",even);
+    console.log("Adults:", adult);
 }
-iseven()
+// isAdults() 
+
+function iseven() {
+    let even = age.filter(value => {
+        return value % 2 == 0;
+    });
+    console.log("Even numbers:", even);
+}
+// iseven()
+
+// Reference type
+
+let items = [
+    { id: 1, name: "Android Mobiles", cost: "9000" },
+    { id: 1, name: "Iphone Mobiles", cost: "25000" },
+    { id: 1, name: "Windows Mobiles", cost: "11000" },
+    { id: 1, name: "Ipad", cost: "20000" },
+    { id: 1, name: "Mobiles", cost: "15000" },
+];
+
+function lessPrice() {
+    let price = items.filter(value => {
+        return value.cost < 12000;
+    })
+    console.log(price);
+}
+lessPrice()
