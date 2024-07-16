6a0667b3bc0b3d04e1d6d7297e936106db5b334c
ajay1931 <ajayprakash032@gmail.com>
2024-07-16 12:34:15 +0530

    renamed

--- maping.js
+++ maping.js
@@ -4,7 +4,7 @@ const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function arrayMap() {
     let maping = number.map(val => {
-        return val * 2, val * 3;
+        return val * 3;
     });
     console.log(maping);
 }
