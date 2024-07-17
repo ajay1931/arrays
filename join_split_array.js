754789e91db14738e122b7cb17ae2f52e557986e
ajay1931 <ajayprakash032@gmail.com>
2024-07-17 20:19:03 +0530

    updated

--- join_split_array.js
+++ join_split_array.js
@@ -7,23 +7,25 @@ function joinArray(){
     let routine = dailyRoutine.join("'s ");
     console.log(routine);
 }
-joinArray()
+joinArray() 
+ //Result =>wake up's eat's have fun's sleep 
 
 // Spliting of arrays
 function splitArray(){
 let userName = fullName.split(" ")  //we can split between the gap of the name
-let firstName =userName[0];
-let lastName = userName[1];
+let firstName =userName[0];  // =>Ajay 
+let lastName = userName[1];  // =>prakash 
 console.log(`my first name is ${firstName} and my last name is ${lastName}`);
 
 }
-// splitArray()
-
+splitArray()
+  //Result =>my first name is Ajay and my last name is prakash 
 
 // split and join
 function splitJoin(){
-    let postSlug = postTittle.split(" ");
+    let postSlug = postTittle.split(" ");  // =>["This", "is", "my", "post"]
     let finalslug = postSlug.join("_")
     console.log(finalslug);
 }
-// splitJoin()
+splitJoin()
+  //Result =>This_is_my_post 
