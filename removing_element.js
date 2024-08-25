43a10f0aa33396f29bcc8f4b1c0732ebef4e3e40
ajay1931 <ajayprakash032@gmail.com>
2024-08-25 10:01:14 +0530

    update

--- removing_element.js
+++ removing_element.js
@@ -7,18 +7,18 @@ function removeElement() {
     number.pop()
     console.log(number);
 }
-// removeElement()
+removeElement()  //  [1, 2, 3, 4,5, 6, 7, 8]
 
                      //shift method
 function removeElement1() {
     number.shift()
     console.log(number);
 }
-// removeElement1()
+removeElement1()  //  [2, 3, 4, 5, 6, 7, 8, 9]
 
                       //splice method
 function removeElement2(){
     number.splice(3,2)
     console.log(number);
 }
-removeElement2()
+removeElement2()  //[1, 2, 3, 4, 7, 8, 9]
