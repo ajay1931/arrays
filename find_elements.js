761defb028f66480702cbbeb082e39de55f30d52
ajay1931 <ajayprakash032@gmail.com>
2024-08-25 10:00:30 +0530

    update

--- find_elements.js
+++ find_elements.js
@@ -1,19 +1,19 @@
 // Finding elements in javascript array
 
 let items = ["apple", "grape", "orange", "banana", "pinapple"]
-// console.log(items.indexOf("orange"));
+console.log(items.indexOf("orange"));  //2
 
 let fruit = ["apple", "grape", "orange", "banana", "pinapple", "apple"]
-// console.log(fruit.lastIndexOf("apple"));
+console.log(fruit.lastIndexOf("apple"));  //5
 
 let items1 = ["apple", "grape", "orange", "banana", "pinapple"]
-// console.log(items1.indexOf("banana") !==-1);
-// console.log(items1.indexOf("red") !==-1);
+console.log(items1.indexOf("banana") !==-1);  //true
+console.log(items1.indexOf("red") !==-1);  //false
 
 // if anything is not given in the array "code run to -1"
 
 let fruit1 = ["apple", "grape", "orange", "banana", "pinapple", "apple"]
-console.log(fruit1.includes("banana"));
-console.log(fruit1.includes("red"));
+console.log(fruit1.includes("banana"));  //true
+console.log(fruit1.includes("red"));  //false
 
 
