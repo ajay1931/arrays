3ec45b6f513c7ae1c83620f5aa1f76cd832be6ac
ajay1931 <ajayprakash032@gmail.com>
2024-08-25 10:00:50 +0530

    update

--- iterating_elements.js
+++ iterating_elements.js
@@ -1,24 +1,45 @@
-const dailyRoutine=["wake up","eat","have fun","sleep"]
+const dailyRoutine = ["wake up", "eat", "have fun", "sleep"]
 
 // For of loop
-function routine1(){
-    for(let Routine of dailyRoutine)
+function routine1() {
+    for (let Routine of dailyRoutine)
         console.log(Routine);
 }
-// routine1()
+routine1()
+// OUTPUT =>
+// wake up
+// eat
+// have fun
+// sleep   
 
 // For in loop
-function routine2(){
-    for(let Routine in dailyRoutine)
-        console.log(Routine,".",dailyRoutine[Routine]);
+function routine2() {
+    for (let Routine in dailyRoutine)
+        console.log(Routine, ".", dailyRoutine[Routine]);
 }
-// routine2()
+routine2()
+// OUTPUT =>
+// 0 . wake up
+// 1 . eat
+// 2 . have fun
+// 3 . sleep
 
 // For each 
-dailyRoutine.forEach(function(routine,index){
+dailyRoutine.forEach(function (routine, index) {
     console.log(index,routine);
-})
+});
+// OUTPUT =>
+//   0 wake up
+//   1 eat
+//   2 have fun
+//   3 sleep
 
 // () => {}  this is fat array key function
-dailyRoutine.forEach((routine,index) =>{
-    console.log(index,routine);})
+dailyRoutine.forEach((routine, index) => {
+    console.log(index, routine);
+});
+// OUTPUT =>
+//   0 wake up
+//   1 eat
+//   2 have fun
+//   3 sleep
