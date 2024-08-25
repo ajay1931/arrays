9ca8e22dbb7a1e8797888eeafaedb00d824fdcdf
ajay1931 <ajayprakash032@gmail.com>
2024-08-25 10:00:07 +0530

    update

--- combine_extract_array.js
+++ combine_extract_array.js
@@ -1,24 +1,37 @@
 let shopping_cart = ["carrot", "beans", "beetroot",];
 let additional_cart = ["coliflower", "ladiesfinger"];
 
-// Combine array
+
+                     // Combine array
 function addon() {
     let totalItems = shopping_cart.concat(additional_cart);
     console.log(totalItems);
 }
-// addon()
+addon()  // Result=>[ 'carrot', 'beans', 'beetroot', 'coliflower', 'ladiesfinger' ]
 
-// Extract array
+
+                     // Extract array
 function Extracton() {
     let totalItems = shopping_cart.concat(additional_cart);
     let extractitems = totalItems.splice(1, 3);
     console.log(extractitems);
 }
-// Extracton()
+Extracton()  // Result=>[ 'beans', 'beetroot', 'coliflower' ]
 
-// spread operater
-function toCombine(){
-    let totalItems =[...shopping_cart,"mint",{name:"ajay",age:24},...additional_cart];
+
+                     // spread operater
+function toCombine() {
+    let totalItems = [...shopping_cart, "mint", { name: "ajay", age: 24 }, ...additional_cart];
     console.log(totalItems);
 }
 toCombine()
+// OUTPUT =>
+//     [
+//         'carrot',
+//         'beans',
+//         'beetroot',
+//         'mint',
+//         { name: 'ajay', age: 24 },
+//         'coliflower',
+//         'ladiesfinger'
+//       ]
